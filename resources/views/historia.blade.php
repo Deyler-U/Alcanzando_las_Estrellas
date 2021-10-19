40d5f5760b37a4b25881ce2502a0215862a2b092
Beneq <bquibc@miumg.edu.gt>
2021-10-19 15:10:24 -0600

    acerca de

--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -2,7 +2,88 @@
 
 @section('content')
 
+    <!-- Header -->
+    <header id="header" class="ex-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Acerca de la asociacion</h1>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </header> <!-- end of ex-header -->
+    <!-- end of header -->
 
+    <div class="inner-sec-layer">					
+					<div class="row middle-grids">
+						<div class="col-lg-6 col-md-6 service-in text-center">
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-desktop mb-4"></span>
+									<h5 class="card-title text-uppercase">Mision</h5>
+									<p class="card-text">Proveemos a nuestros participantes y cooperantes servicios de excelencia, que contribuyan al desarrollo integral de las familias participantes para que sean capaces de mejorar su calidad de vida.</p>
+								</div>
+							</div>
+						</div>
+						 <div class="col-lg-6 col-md-6 service-in text-center ">
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-desktop mb-4"></span>
+									<h5 class="card-title text-uppercase">Vision</h5>
+									<p class="card-text">Ser una organización con sede en Cobán, Alta Verapaz, sostenible e integra; impulsando proyectos en beneficio de la niñez y adolescencia, con calidad humana, en constante mejoramiento; reconocida por la excelencia y aptitud de servicio, que inciden en el cambio de vida de las personas más vulnerables.</p>
+								</div>
+							</div>
+						</div>					
+					</div>
+				</div>
+
+
+    <!-- historia-->
+    <div class="ex-basic-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="text-container">
+                        <h3>ASOCIACION ALCANZANDO ESTRELLAS – ANTES PASTORAL DE LA NIÑEZ</h3>
+                        <p>En 1994 se inició como institución de la iglesia católica, en busca de respuesta a las necesidades de los niños de la calle que ninguna institución atendía. Con terapia de juego, refuerzo positivo y reflexiones.  Este grupo se dejó de atender en mayo 1997.
+                        </p>
+						<p>
+						De Inmediato se apoyó la educación formal a niñez de familias en pobreza y pobreza extrema para contrarrestar y prevenir la problemática de la calle. El grupo se fue incrementando y en 1997 se decide concentrar esfuerzos en la atención del grupo de prevención, como se le llamo a los niños escolarizados de familias en pobreza y pobreza extrema que asistían a escuelas públicas.
+						</p>
+						<p>
+						 2001 al 2004 se ejecuta proyecto Brocoly para contrarrestar el trabajo infantil en Chilasco Baja Verapaz.
+						 </p>
+						<p>
+						1998 al 2001 se atendieron centros de Cenacep o centros de aprestamiento acelerado dirigidos a niños que no han asistido a preprimaria, con   6 y 8 centros por año en colonias marginales de Cobán.
+						</p>
+						<p>
+						Durante los años de 1997 al 2000 se implementó   la atención a la niñez trabajadora, la niñez que lustra, vende periódico o golosinas: alfabetizándola e incorporándola a la educación formal. Se ejecutó el proyecto de becas e iniciaron en vacaciones talleres de dibujo, música, teatro, así como técnicas de afición a la lectura.
+						Simultáneamente se inició trabajo con las madres y padres, sistemáticamente la “Escuela de Madres”, esto desde en 1996 y a partir del 2000 con los Padres. así como asesoría y atención a problemas familiares, asuntos legales (violencia intrafamiliar), asentamientos tardíos, otros y acompañamiento en gestiones de diversa índole. También talleres de capacitación laboral a madres, padres y niños mayores de 14 años como panadería, elaboración de alimentos, manualidades, fontanería y además alfabetización a madres y padres.
+						Desde el 2011 se brinda atención psicológica, a niñas, niños, adolescentes, madres y padres.
+						</p>
+						<p>
+						Asimismo, se a brindado atención en salud para la población objetivo desde 1998 se ha coordinado con diferentes instituciones, Escuela de enfermería, centros de salud y universidades Además, se han realizado otras actividades importantes como: Realización de Auto-diagnósticos institucionales con la metodología de I-STAR a varias instituciones.
+						</p>
+						<p>
+						En 1996 realizaron capacitaciones a maestros de escuelas públicas por distritos, en Filosofía para Niños (metodología dialógica que enseña a pensar desde la niñez) y en 2001 nuevamente con maestros de escuelas del área urbana esta vez con el compromiso de aplicación en el aula. Del 2001 al 2004 se realizaron talleres a maestros en San Rafael, Chilasco en la metodología Activa o Nueva Escuela Unitaria.
+						</p>
+						<p>
+						Durante los años de 1998, 2001 y 2002, se ejecuta jornada de prevención de la Violencia con foros de sensibilización, a diferentes grupos, mensajes radiales sobre la importancia de la educación, como parte del Grupo de Trabajo Infantil. Desayunos informativos con los medios de comunicación y como miembros del Foro de Niñez de la calle, se ejecutó la campaña de sensibilización: <b>No llames al niño de Calle</b>, en Cobán.
+						</p>
+						<p>
+						Con la población en general se ha apoyado en casos concretos de maltrato infantil, con denuncia y rescate a niños maltratados así también casos de violaciones en cualquiera de los derechos del niño. Denuncia y seguimiento en desapariciones y arbitrariedades con los niños de familias pobres e indígenas, coordinado con Derechos Humanos, PGN, y otros.
+						</p>
+						<p>
+						<b>Participación e incidencia política:</b> se ha participado en diferentes redes o grupos de trabajo: como.  1994 en el comité pro-convención; 1997 y 1998 en movimiento social por los derechos de la niñez, foro de la niñez de la calle, grupo Trabajo infantil (ONG´s, que trabajaban con la niñez trabajadora); 2000 al 2007 Miembros de Coordinadora de ONG´s de A.V.; 2001 a la fecha Miembros del Centro de Justicia de A.V. en Comisión  de Prevención de la Violencia intrafamiliar ; 2004 al 2005 miembro de la Junta de Protección de la niñez y Juventud adscrita a D.D.H.H.; 2006 al 2008 Miembro de ACIEAV,  Alianza Cooperativa para la Incidencia en Educación en A.V.; 2007 a la fecha Miembro de la Red de Protección de la Niñez y Juventud de A.V.
+						En 2012 como una estrategia de fortalecimiento institucional, se inician trámites para la conformación en asociación lo que se logra en 2014, por lo que en enero de 2015 se inició el trabajo como Asociación Alcanzando Estrellas, pero con la misma población objetivo, los mismos, fines y mística de trabajo.
+						</p>
+						       
+                </div> <!-- end of col-->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-2 -->
+    <!-- end of privacy content -->
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,8 @@
     <link href="{{asset('css/fontawesome-all.css')}}" rel="stylesheet">
     <link href="{{asset('css/swiper.css')}}" rel="stylesheet">
     <link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet">
-    <link href="{{asset('css/styles.css')}}" rel="stylesheet">
-    
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet">   
+
     <!-- Favicon  -->
     <link rel="icon" href="images/asaesico.png">
 </head>
@@ -182,15 +182,15 @@
     
         
     <!-- Scripts -->
-    <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
-    <script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
-    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
-    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
-    <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
-    <script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
-    <script src="js/morphext.min.js"></script> <!-- Morphtext rotating text in the header -->
-    <script src="js/isotope.pkgd.min.js"></script> <!-- Isotope for filter -->
-    <script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
-    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+    <script src="{{asset('js/jquery.min.js')}}"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="{{asset('js/popper.min.js')}}"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script> <!-- Bootstrap framework -->
+    <script src="{{asset('js/jquery.easing.min.js')}}"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="{{asset('js/swiper.min.js')}}"></script> <!-- Swiper for image and text sliders -->
+    <script src="{{asset('js/jquery.magnific-popup.js')}}"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="{{asset('js/morphext.min.js')}}"></script> <!-- Morphtext rotating text in the header -->
+    <script src="{{asset('js/isotope.pkgd.min.js')}}"></script> <!-- Isotope for filter -->
+    <script src="{{asset('js/validator.min.js')}}"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="{{asset('js/scripts.js')}}"></script> <!-- Custom scripts -->
 </body>
 </html>
